188d473a2a26b41f6d1777d0af8e3c20581cc64c
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-04-28 18:15:37 +1000

    Set up mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,23 @@
-import express, { response } from "express";
-import { PORT } from "./config.js";
+import express from "express";
+import { mongoDBURL, PORT } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
 //Listen middleware. It tells whether server is running or not.
-app.listen(PORT, () => {
-    console.log(`App is running on port: ${PORT}`);
-});
+mongoose.connect(mongoDBURL)
+    .then(() => {
+        console.log("App connected to database");
+        app.listen(PORT, () => {
+            console.log(`App is running on port: ${PORT}`);
+        });
+    }).catch((err) => {
+        console.log(err)
+    });
 
 //Get from root directory
 app.get('/', (req, res) => {
     console.log(req.headers);
     return res.status(200).send("Welcome to MERN stack");
 });
+
