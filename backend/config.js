5febf7c0641644511faa3d141c460f210d8e2d97
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-04-28 18:18:51 +1000

    Edit MongoDB password

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-    'mongodb+srv://sigmap:sigmap@cluster0.uxsaxla.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    'mongodb+srv://sigmap:DenielMongoose61224514II94@cluster0.uxsaxla.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
