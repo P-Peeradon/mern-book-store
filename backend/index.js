940852cdeb0b06e7d69325c8b34044626c339805
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-04-28 18:04:09 +1000

    GET root directory

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
-import express from "express";
+import express, { response } from "express";
 import { PORT } from "./config.js";
 
 const app = express();
 
+//Listen middleware. It tells whether server is running or not.
 app.listen(PORT, () => {
-    console.log(`App is running on port: ${PORT}`)
-})
+    console.log(`App is running on port: ${PORT}`);
+});
+
+//Get from root directory
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(234).send("Welcome to MERN stack");
+});
