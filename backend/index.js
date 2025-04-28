03b5d66c48eac3e4a5144e938d139b453b28bcb1
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-04-28 18:36:01 +1000

    MongoDB CRUD

--- backend/index.js
+++ backend/index.js
@@ -21,3 +21,48 @@ app.get('/', (req, res) => {
     return res.status(200).send("Welcome to MERN stack");
 });
 
+//Get all books from MongoDB
+app.get('/books', async (req, res) => {
+    try {
+        const books = await Book.find({});
+
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({msg: err});
+    }
+});
+
+//Get one book by id from MongoDB
+app.get('/books/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const book = await Book.findById(id);
+
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send('Problem')
+    }
+});
+
+//Post book
+app.post('/books', async (req, res) => {
+    try {
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear
+        };
+        
+        const book = await Book.create(newBook);
+
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send('Problem')
+    }
+});
+
